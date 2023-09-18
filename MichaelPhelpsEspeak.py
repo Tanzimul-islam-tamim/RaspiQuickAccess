2ac5418f79d7e8cea80969007345f6568ce354dc
Tamim <71815921+Tanzimul-islam-tamim@users.noreply.github.com>
2023-09-18 11:48:31 +0600

    Add files via upload

---
+++ MichaelPhelpsEspeak.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("espeak 'Hello Mr. Michael Phelps  Your 100 meters backstroke relay race is tomorrow afternoon at 1630 hours  It will initiate on XPool OS 341  You would like me to take you there'")
